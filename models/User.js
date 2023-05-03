29bf12eab1bd98ca2dfc0508906d86917f7910bb
dagger-v <goingarcher@gmail.com>
2023-05-03 02:33:19 -0700

    added login

--- models/User.js
+++ models/User.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+// Create User Schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// hash password
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,69 @@
 var express = require("express");
 var router = express.Router();
 
+const User = require("../models/User");
+const passport = require("passport");
+
+// create passport local strategy
+passport.use(User.createStrategy());
+
+// Serialize and deserialize user
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// Register user in DB
+router.post("/users/register", async (req, res) => {
+  try {
+    // Register user
+    const registerUser = await User.register(
+      { username: req.body.username },
+      req.body.password
+    );
+    if (registerUser) {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
+    } else {
+      res.redirect("/register");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// Login user
+router.post("/users/login", (req, res) => {
+  // Create new user object
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  // Using passport will check credentials
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+// Logout user
+router.get("/logout", (req, res) => {
+  // Use passport logout method
+  req.logout();
+  res.redirect("/");
+});
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -8,6 +71,10 @@ router.get("/", function (req, res, next) {
 
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
