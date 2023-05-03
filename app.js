5dafa481363977ebcd43703e941292c2e5493c4e
dagger-v <goingarcher@gmail.com>
2023-05-03 01:41:56 -0700

    added register page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const passport = require("passport");
 
-var app = express();
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,8 +23,33 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// setup session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+
+// use passport to deal with session
+app.use(passport.session());
+
+// connect to DB
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DB_CONNECT;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
